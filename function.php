045eac09535c26db11617a4e57e5a01765c9bb8b
Mike <m.courbesriviere@orange.fr>
2024-12-18 20:44:34 +0100

    mise a jour de mes fontions test & mise en place d une verif de methode GET

--- function.php
+++ function.php
@@ -6,24 +6,35 @@ function checkIt(string $name, string $value, array $data): string // les parame
 {
     $attributes = '';  // l'attribut est une variable creer ici pour verifier l'etat checked
     // lorsque le formulaire arrive l'etat est vide
+
     if (isset($data[$name]) && in_array($value, $data[$name])) { // si le tableau data est different de vide et que la valeur de l'attribut name est different de vide
-        $attributes .= 'checked';// alors on place l'attribut checked
-    } 
+        $attributes .= 'checked'; // alors on place l'attribut checked
+    }
     return <<<HTML
     <input type="checkbox" name="{$name}[]" value="{$value}" $attributes>
 HTML;
 }
 
-function checkedRadio(string $name, string $value): string // les parametres de la function sont placer ici
+function checkedRadio(string $name, string $value, array $data, bool $isFirst = false): string
 {
-    $attributes = ''; 
+    $attributes = '';
+
+    // Sélectionner le premier élément si $_GET est vide et c'est la première option
+    if (empty($data) && $isFirst) {
+        $attributes = 'checked';
+    }
+    // Sélectionner la valeur dans $_GET si présente
     if (isset($data[$name]) && $data[$name] === $value) {
-        $attributes .= 'checked';
-    }    
+        $attributes = 'checked';
+    }
+
     return <<<HTML
     <input type="radio" name="$name" value="$value" $attributes>
 HTML;
 }
+
+
+
 
 
 // 1 possede l'attribut 
--- jeu.php
+++ jeu.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,65 +10,66 @@
     <!--²lien avec mon fchier css -->
     <link rel="stylesheet" href="CSS/style.css">
 </head>
+
 <body>
 
-<?php require_once 'header.php'; ?>
-<?php require_once 'function.php'; ?>
-<?php  
-$aDeviner = 150;
-$erreur= null;
-$succes = null;
-$value =null;
-if (isset($_GET['chiffre'])) {
-    if ($_GET['chiffre'] > $aDeviner) {
-        $erreur = 'Votre chiffre est trop grand';
-    } elseif ($_GET['chiffre'] < $aDeviner) {
-        $erreur = 'Votre chiffre est trop petit';
-    } else {
-        $succes = 'Bravo! Vous avez deviné le chiffre.';
+    <?php require_once 'header.php'; ?>
+    <?php require_once 'function.php'; ?>
+    <?php
+    $aDeviner = 150;
+    $erreur = null;
+    $succes = null;
+    $value = null;
+    if (isset($_GET['chiffre'])) {
+        if ($_GET['chiffre'] > $aDeviner) {
+            $erreur = 'Votre chiffre est trop grand';
+        } elseif ($_GET['chiffre'] < $aDeviner) {
+            $erreur = 'Votre chiffre est trop petit';
+        } else {
+            $succes = 'Bravo! Vous avez deviné le chiffre.';
+        }
+        $value = (int)$_GET['chiffre'];
     }
-    $value = (int)$_GET['chiffre'];
-}
-?>
-<?php if ($erreur): ?>
-    <div class="alert alert-danger">
-        <?= $erreur ?>
-    </div>
-<?php elseif ($succes): ?>
-    <div class="alert alert-success">
-        <?= $succes ?>
-    </div>
-<?php endif ?>
+    ?>
+    <?php if ($erreur): ?>
+        <div class="alert alert-danger">
+            <?= $erreur ?>
+        </div>
+    <?php elseif ($succes): ?>
+        <div class="alert alert-success">
+            <?= $succes ?>
+        </div>
+    <?php endif ?>
 
-<form action="jeu.php" method="GET" >
-    <div class="form-group">
-    <input type="number" class="form-control" name="chiffre" placeholder="entre 0 et 1000" value="<?= $value ?>">
-    </div>
-<!--autre methode possible pour eviter que l'utilisateur ne puisse pas passer par le url
+    <form action="jeu.php" method="GET">
+        <div class="form-group">
+            <input type="number" class="form-control" name="chiffre" placeholder="entre 0 et 1000" value="<?= $value ?>">
+        </div>
+        <!--autre methode possible pour eviter que l'utilisateur ne puisse pas passer par le url
 n <input type="number" name="chiffre" placeholder="entre 0 et 1000" values=" htmlentities($_GET['chiffre']) -->
-    <button type=submit class="btn btn-primary">Deviner</button>
-</form>
-<br>
-<br>
-<br>
-<?php
-$parfums=[
-    'vanille' => 3,
-    'fraise' => 5,
-    'chocolat' => 2
-];
-$cornets = [
-    'pot' => 2,
-    'cornet' => 3
-];
+        <button type=submit class="btn btn-primary">Deviner</button>
+    </form>
+    <br>
+    <br>
+    <br>
+    <?php
+    $parfums = [
+        'vanille' => 3,
+        'fraise' => 5,
+        'chocolat' => 2
+    ];
+    $cornets = [
+        'pot' => 2,
+        'cornet' => 3
+    ];
 
-$supplements = [
-    'pepites' => 1,
-    'chantilly' => 0.5
-];
+    $supplements = [
+        'pepites' => 1,
+        'chantilly' => 0.5
+    ];
 
-?>
-<!--
+    ?>
+    <!--
     <div class="form-group"> les checkbox me permettent de faire des selections multiple je doit creer une function pour garder les choix des utilisateurs
         <input type="checkbox" name = "parfum[]" value="vanille">Vanille
         <input type="checkbox" name = "parfum[]" value="fraise">Fraise
@@ -82,43 +84,48 @@ $supplements = [
         <input type="checkbox" name = "supplements" value="chantilly">Chantilly
     </div>
     -->
-<!--la zone ci dessus peut etre retravailler de cette maniere avec du php -->  
-<form action="/jeu.php" method="GET">
+    <!--la zone ci dessus peut etre retravailler de cette maniere avec du php -->
+    <form action="/jeu.php" method="GET">
+        <?php
+        // Vérification des données dans $_GET pour afficher un message d'erreur
+        if ($_SERVER['REQUEST_METHOD'] === 'GET' && empty($_GET['parfum'])): ?>
+            <div class="alert alert-danger">
+                Vous n'avez sélectionné aucun parfum.
+            </div>
+        <?php endif; ?>
+
         <?php foreach ($parfums as $parfum => $prix): ?>
             <div class="checkbox">
                 <label>
-                    <?= 
-                    checkIt('parfum', $parfum, $_GET); 
-                    ?> 
+
+                    <?=
+                    checkIt('parfum', $parfum, $_GET);
+                    ?>
                     <?= $parfum; ?> - <?= $prix; ?> €
                 </label>
             </div>
         <?php endforeach; ?>
-        <?php var_dump($_GET); ?>
         <?php foreach ($cornets as $cornet => $prix): ?>
             <div class="checkbox">
-                <label>
-                    <?= 
-                    checkedRadio('cornet', $cornet, $_GET); 
-                    ?> 
-                    <?= $cornet; ?> - <?= $prix; ?> €
-                </label>
+                <?=
+                checkedRadio('cornet', $cornet, $_GET);
+                ?>
+                <?= $cornet; ?> - <?= $prix; ?> €
             </div>
         <?php endforeach; ?>
-        <?php var_dump($_GET); ?>
         <?php foreach ($supplements as $supplement => $prix): ?>
             <div class="checkbox">
                 <label>
-                    <?= 
-                    checkIt('supplement', $supplement, $_GET); 
-                    ?> 
+                    <?=
+                    checkIt('supplement', $supplement, $_GET);
+                    ?>
                     <?= $supplement; ?> - <?= $prix; ?> €
                 </label>
             </div>
         <?php endforeach; ?>
-    <button type="submit" class="btn btn-primary">Commander</button>
-</form> 
-<?php require_once 'footer.php' ?>
+        <button type="submit" class="btn btn-primary">Commander</button>
+    </form>
+    <?php require_once 'footer.php' ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
